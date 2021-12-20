6ab890a4f4cf2f9fc252dc455c24afc89b82565f
Joey Toet <joeytoet14@gmail.com>
2021-12-20 09:23:48 +0100

    Nieuw MVC project aangemaakt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _8A_WDPR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
