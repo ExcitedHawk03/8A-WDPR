e2163ec88d57f568877d15abef0f7c6c4bde6565
Joey Toet <joeytoet14@gmail.com>
2022-01-16 10:35:55 +0100

    Lay-out verbeterd en meer accessible gemaakt

---
+++ WebApplicatie/Controllers/Orthopedagoogcontroller.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class OrthopedagoogController : Controller
+{
+    public IActionResult Laura ()
+    {
+        return View ();
+    }
+
+    public IActionResult Takezo ()
+    {
+        return View ();
+    }
+
+    public IActionResult Jan ()
+    {
+        return View ();
+    }
+
+    public IActionResult Dagmar ()
+    {
+        return View ();
+    }
+}
